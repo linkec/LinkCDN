5024f025e8ca35c84b63df079be43f7210ca7139
Linkec <linkec@live.com>
2018-10-26 14:31:44 +0800

    update signin tpl

--- tpl/default/account/signin.tpl.php
+++ tpl/default/account/signin.tpl.php
@@ -1,190 +1,157 @@
-<?php !defined('IN_APP') && exit('[XDDrive] Access Denied!'); ?>
+<?php !defined('IN_APP') && exit('[LinkCDN] Access Denied!'); ?>
 <!DOCTYPE html>
-<html lang="en">
-
+<html dir="ltr" lang="zh_cn">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>登录控制台 | 睿速 - 睿智的加速选择</title>
-
-
-    <!--STYLESHEET-->
-    <!--=================================================-->
-
-    <!--Open Sans Font [ OPTIONAL ]-->
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
-
-
-    <!--Bootstrap Stylesheet [ REQUIRED ]-->
-    <link href="{$static_url}/css/bootstrap.min.css" rel="stylesheet">
-
-
-    <!--Nifty Stylesheet [ REQUIRED ]-->
-    <link href="{$static_url}/css/nifty.min.css" rel="stylesheet">
-
-
-    <!--Nifty Premium Icon [ DEMONSTRATION ]-->
-    <link href="{$static_url}/css/demo/nifty-demo-icons.min.css" rel="stylesheet">
-
-
-        
-    <!--Demo [ DEMONSTRATION ]-->
-    <link href="{$static_url}/css/demo/nifty-demo.min.css" rel="stylesheet">
-
-
-    <!--Magic Checkbox [ OPTIONAL ]-->
-    <link href="{$static_url}/plugins/magic-check/css/magic-check.min.css" rel="stylesheet">
-
-    <!--Bootstrap Validator [ OPTIONAL ]-->
-    <link href="{$static_url}/plugins/bootstrap-validator/bootstrapValidator.min.css" rel="stylesheet">
-
-
-
-
-
-
-    
-    <!--JAVASCRIPT-->
-    <!--=================================================-->
-
-    <!--Pace - Page Load Progress Par [OPTIONAL]-->
-    <link href="{$static_url}/plugins/pace/pace.min.css" rel="stylesheet">
-    <script src="{$static_url}/plugins/pace/pace.min.js"></script>
-
-
-    <!--jQuery [ REQUIRED ]-->
-    <script src="{$static_url}/js/jquery.min.js"></script>
-
-
-    <!--BootstrapJS [ RECOMMENDED ]-->
-    <script src="{$static_url}/js/bootstrap.min.js"></script>
-
-
-    <!--NiftyJS [ RECOMMENDED ]-->
-    <script src="{$static_url}/js/nifty.min.js"></script>
-    
-    <!--Bootstrap Validator [ OPTIONAL ]-->
-    <script src="{$static_url}/plugins/bootstrap-validator/bootstrapValidator.min.js"></script>
-
-
-
-
-    
-    <!--=================================================
-
-    REQUIRED
-    You must include this in your project.
-
-
-    RECOMMENDED
-    This category must be included but you may modify which plugins or components which should be included in your project.
-
-
-    OPTIONAL
-    Optional plugins. You may choose whether to include it in your project or not.
-
-
-    DEMONSTRATION
-    This is to be removed, used for demonstration purposes only. This category must not be included in your project.
-
-
-    SAMPLE
-    Some script samples which explain how to initialize plugins or components. This category should not be included in your project.
-
-
-    Detailed information and more samples can be found in the document.
-
-    =================================================-->
-        
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Xtreme admin Template - The Ultimate Multipurpose admin template</title>
+    <!-- Custom CSS -->
+    <link href="{STATIC_PATH}/dist/css/style.min.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
 
-<!--TIPS-->
-<!--You may remove all ID or Class names which contain "demo-", they are only used for demonstration. -->
-
 <body>
-	<div id="container" class="cls-container">
-		
-		<!-- BACKGROUND IMAGE -->
-		<!--===================================================-->
-		<div id="bg-overlay"></div>
-		
-		
-		<!-- LOGIN FORM -->
-		<!--===================================================-->
-		<div class="cls-content">
-		    <div class="cls-content-sm panel">
-		        <div class="panel-body">
-		            <div class="mar-ver pad-btm">
-		                <h3 class="h4 mar-no">RayCDN Console</h3>
-		                <p class="text-muted">登录到控制台</p>
-		            </div>
-		            <form id="login-form" action="/account/signin">
-						<input type="hidden" name="task" value="login">
-						<input type="hidden" name="formhash" value="{$formhash}">
-		                <div class="form-group">
-		                    <input type="text" class="form-control" placeholder="注册邮箱" autofocus name="email">
-							<small class="help-block text-danger" style="display:none;" id="email_state"></small>
-		                </div>
-		                <div class="form-group">
-		                    <input type="password" class="form-control" placeholder="密码" name="password">
-							<small class="help-block text-danger" style="display:none;" id="pwd_state"></small>
-		                </div>
-		                <button class="btn btn-primary btn-lg btn-block" type="submit">登录</button>
-		            </form>
-		        </div>
-		
-		        <div class="pad-all">
-		            <a href="/account/forget" class="btn-link mar-rgt">忘记密码 ？</a>
-		            <a href="/account/signup" class="btn-link mar-lft">创建新的帐号</a>
-		
-		            <div class="media pad-top bord-top">
-		                <div class="media-body text-center">
-		                    RayCDN Ltd. @2017
-		                </div>
-		            </div>
-		        </div>
-		    </div>
-		</div>
-		<!--===================================================-->
-		
-		
-	</div>
-	<!--===================================================-->
-	<!-- END OF CONTAINER -->
-		<script type="text/javascript">
-			$('#login-form').bind('submit',function(){
-					var form = $('#login-form');
-					$('button').attr('disabled',"true");
-					  $.ajax({
-							url: form.attr('action'),
-							type: "POST",
-							data: form.serialize(),
-							dataType:'json',
-							success: function (data) {
-							  if(data.status=='success'){
-								  location = '/home';
-							  }else{
-								  if(data.email_hint){
-									 $('#email_state').css('display','block');
-									 $('#email_state').html(data.email_hint);
-								  }
-								  if(data.pwd_hint){
-									 $('#pwd_state').css('display','block');
-									 $('#pwd_state').html(data.pwd_hint);
-								  }
-							  }
-							  $('button').removeAttr('disabled',"true");
-							},
-							error: function (jqXhr, textStatus, errorThrown) {
-								$('button').removeAttr('disabled',"true");
-								alert(errorThrown);
-							}
-					  });
-			    event.preventDefault();
-				return false;
-			});
-		</script>
-
-	</body>
+    <div class="main-wrapper">
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Login box.scss -->
+        <!-- ============================================================== -->
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" style="background:url(../../assets/images/big/auth-bg.jpg) no-repeat center center;">
+            <div class="auth-box">
+                <div id="loginform">
+                    <div class="logo">
+                        <span class="db"><img src="../../assets/images/logo-icon.png" alt="logo" /></span>
+                        <h5 class="font-medium m-b-20">Sign In to Admin</h5>
+                    </div>
+                    <!-- Form -->
+                    <div class="row">
+                        <div class="col-12">
+                            <form class="form-horizontal m-t-20" id="loginform" action="index.html">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon1"><i class="ti-user"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control form-control-lg" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon2"><i class="ti-pencil"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control form-control-lg" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                            <label class="custom-control-label" for="customCheck1">Remember me</label>
+                                            <a href="javascript:void(0)" id="to-recover" class="text-dark float-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group text-center">
+                                    <div class="col-xs-12 p-b-20">
+                                        <button class="btn btn-block btn-lg btn-info" type="submit">Log In</button>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 m-t-10 text-center">
+                                        <div class="social">
+                                            <a href="javascript:void(0)" class="btn  btn-facebook" data-toggle="tooltip" title="" data-original-title="Login with Facebook"> <i aria-hidden="true" class="fab  fa-facebook"></i> </a>
+                                            <a href="javascript:void(0)" class="btn btn-googleplus" data-toggle="tooltip" title="" data-original-title="Login with Google"> <i aria-hidden="true" class="fab  fa-google-plus"></i> </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group m-b-0 m-t-10">
+                                    <div class="col-sm-12 text-center">
+                                        Don't have an account? <a href="authentication-register1.html" class="text-info m-l-5"><b>Sign Up</b></a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div id="recoverform">
+                    <div class="logo">
+                        <span class="db"><img src="../../assets/images/logo-icon.png" alt="logo" /></span>
+                        <h5 class="font-medium m-b-20">Recover Password</h5>
+                        <span>Enter your Email and instructions will be sent to you!</span>
+                    </div>
+                    <div class="row m-t-20">
+                        <!-- Form -->
+                        <form class="col-12" action="index.html">
+                            <!-- email -->
+                            <div class="form-group row">
+                                <div class="col-12">
+                                    <input class="form-control form-control-lg" type="email" required="" placeholder="Username">
+                                </div>
+                            </div>
+                            <!-- pwd -->
+                            <div class="row m-t-20">
+                                <div class="col-12">
+                                    <button class="btn btn-block btn-lg btn-danger" type="submit" name="action">Reset</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Login box.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper scss in scafholding.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper scss in scafholding.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Right Sidebar -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Right Sidebar -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- All Required js -->
+    <!-- ============================================================== -->
+    <script src="../../assets/libs/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../../assets/libs/popper.js/dist/umd/popper.min.js"></script>
+    <script src="../../assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugin js -->
+    <!-- ============================================================== -->
+    <script>
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".preloader").fadeOut();
+    // ============================================================== 
+    // Login and Recover Password 
+    // ============================================================== 
+    $('#to-recover').on("click", function() {
+        $("#loginform").slideUp();
+        $("#recoverform").fadeIn();
+    });
+    </script>
+</body>
 </html>
